fed601d99ce50b9ec779e8780310ec6b986619c5
Julia Camacho <49563129+juliacamacho@users.noreply.github.com>
2022-02-19 01:59:42 -0500

    add header/nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,24 @@ import Map from './components/Map';
 
 function App() {
   return (
-    <div className="text-2xl">
-      <div className='h-screen w-3/6'>
-        <Map />
+    <div>
+
+      <div className="bg-blue-500 text-2xl font-bold text-white py-2 px-10">
+        AccCity
       </div>
-      <div className='h-screen w-2/6'>
-        {/* Buttons */}
+
+      <div className="text-2xl">
+        <div className='h-screen w-3/6'>
+          <Map />
+        </div>
+        <div className='h-screen w-2/6'>
+          {/* Buttons */}
+        </div>
+        <div className='h-screen w-1/6'>
+          {/* Score */}
+        </div>
       </div>
-      <div className='h-screen w-1/6'>
-        {/* Score */}
-      </div>
+    
     </div>
   );
 }
