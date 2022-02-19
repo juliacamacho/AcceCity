f47f025f702f16a46f2c52e5379281e95459192a
pmallick1 <44411160+pmallick1@users.noreply.github.com>
2022-02-19 12:02:50 -0500

    Filter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -51,18 +51,26 @@ function App() {
       <div className="grid grid-cols-6">
         <div className='h-screen col-span-3 py-6 px-10'>
           <div className="grid-cols-3 pb-4">
-            <form onSubmit={(e)=>{
+            <form className="flex" onSubmit={(e)=>{
               e.preventDefault();
               searchLoc()
             }}>
             <input 
               type="text"
               id="search"
-              className="col-span-2 w-full shadow focus:ring-blue-500 border-gray-300 px-4 rounded-sm py-2" 
+              className="col-span-2 w-10/12 shadow focus:ring-blue-500 border-gray-300 px-4 rounded-sm py-2" 
               placeholder="Search for a city, street, or address..."
               onChange={(e)=>setLocStr(e.target.value)}
               value={locStr}
               />
+            <select class="px-4 py-3 w-2/12 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm">
+              <option value="">Filter By...</option>
+              <option value="Traffic">Traffic</option>
+              <option value="Walkability">Walkability</option>
+              <option value="Accessbility">Accessbility</option>
+              <option value="Parking">Parking</option>
+            </select>
+              
             </form >
           </div>
           <MapWrapped
